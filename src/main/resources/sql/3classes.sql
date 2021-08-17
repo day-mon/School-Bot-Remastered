99c632c5d9ec92489f6d491d0a9cd7b55bc4d410
damon <lolmemedon@gmail.com>
2021-08-17 01:44:59 -0400

    quick fix

--- src/main/resources/sql/3classes.sql
+++ src/main/resources/sql/3classes.sql
@@ -1,74 +1,28 @@
 create table if not exists classes
 (
-    id
-    bigserial
-    constraint
-    classes_pkey
-    primary
-    key,
-    number
-    bigint,
-    professor_id
-    bigint
-    not
-    null
-    constraint
-    professor_id
-    references
-    professors,
-    preqs
-    text,
-    school_id
-    bigint
-    not
-    null
-    constraint
-    school_id
-    references
-    schools,
-    name
-    text
-    not
-    null,
-    role_id
-    bigint
-    not
-    null,
-    channel_id
-    bigint
-    not
-    null,
-    description
-    text,
-    term
-    text
-    not
-    null,
-    guild_id
-    bigint
-    not
-    null,
-    identifier
-    text
-    not
-    null,
-    location
-    text,
-    start_date
-    date
-    not
-    null,
-    end_date
-    date
-    not
-    null,
-    time
-    text,
-    level
-    text,
-    room
-    text,
-    autofilled
-    boolean
+    id           bigserial
+    constraint classes_pkey
+    primary key,
+    number       bigint,
+    professor_id bigint not null
+    constraint professor_id
+    references professors,
+    preqs        text,
+    school_id    bigint not null
+    constraint school_id
+    references schools,
+    name         text    not null,
+    role_id      bigint  not null,
+    channel_id   bigint  not null,
+    description  text,
+    term         text    not null,
+    guild_id     bigint  not null,
+    identifier   text    not null,
+    location     text,
+    start_date   date    not null,
+    end_date     date    not null,
+    time         text,
+    level        text,
+    room         text,
+    autofilled   boolean
 );
-
